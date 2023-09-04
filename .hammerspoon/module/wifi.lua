077ea33e485b0409d5ca8535b3d20328f6184555
MirageTurtle <mirageturtle@gmail.com>
2023-09-04 13:40:31 +0800

    (.hammerspoon)Add wifi support.

---
+++ .hammerspoon/module/wifi.lua
@@ -0,0 +1,24 @@
+-- Based on hammerspoon-config
+-- Wifi management
+-- Manage others(e.g., window) based on wifi SSID.
+
+function ssidChangedCallback()
+    -- I want to test network when I just open my Mac.
+    ssid = hs.wifi.currentNetwork()
+    if (ssid ~= nil) then
+        print("ssid = " .. ssid)
+        if (ssid == "NotFound") then
+            hs.notify.new({title="Hammerspoon", informativeText="You are in office. Mute now.", autoWithdraw=true, withdrawAfter=3}):send()
+            hs.audiodevice.defaultOutputDevice():setVolume(0)
+            hs.audiodevice.defaultOutputDevice():setMuted(true)
+        end
+        if (ssid == "eduroam") then
+            -- TODO: Test network connection and if need, reconnect it.
+            hs.notify.new({title="Hammerspoon", informativeText="You are in eduroam."}):send()
+        end
+    end
+
+end
+
+wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
+wifiWatcher:start()
