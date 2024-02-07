82782aaccaa41873f5a33b345441c0aa9d09f072
MirageTurtle <mirageturtle@gmail.com>
2024-02-07 14:22:07 +0800

    Basic configuration for lsp-bridge
    Some issue still.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -173,7 +173,9 @@
 ;; lsp-brige dependency
 (use-package yasnippet
   :ensure t
-  :init (yas-global-mode 1))
+  :straight t
+  :config
+  (yas-global-mode 1))
 
 (use-package lsp-bridge
   :straight '(lsp-bridge :type git :host github :repo "manateelazycat/lsp-bridge"
@@ -182,10 +184,20 @@
   :ensure t
   :init
   (global-lsp-bridge-mode)
-  ;; :config
+  :config
   ;; (setq lsp-bridge-python-lsp-server 'pyright)
+  (setq lsp-bridge-enable-log t)
   (setq lsp-bridge-python-command (expand-file-name "~/Documents/venv/lsp-bridge/bin/python3"))
   (setq acm-enable-copilot t))
+
+;; Some Issue
+;; https://github.com/manateelazycat/lsp-bridge/blob/master/README.md
+(defun enable-lsp-bridge()
+  (when-let* ((project (project-current))
+              (project-root (nth 2 project)))
+    (setq-local lsp-bridge-user-langserver-dir project-root
+                lsp-bridge-user-multiserver-dir project-root))
+  (lsp-bridge-mode))
 
 ;; (use-package flycheck
 ;;   :ensure t
@@ -339,6 +351,30 @@
   (setenv "WORKON_HOME" (expand-file-name "~/Documents/venv"))
   ;; (setq python-shell-interpreter "python3")
   (pyvenv-mode t))
+
+;; for lsp-bridge
+(defun local/lsp-bridge-get-single-lang-server-by-project (project-path filepath)
+  (let* ((json-object-type 'plist)
+         (custom-dir (expand-file-name ".cache/lsp-bridge/pyright" user-emacs-directory))
+         (custom-config (expand-file-name "pyright.json" custom-dir))
+         (default-config (json-read-file (expand-file-name "straight/build/lsp-bridge/langserver/pyright.json" user-emacs-directory)))
+         (settings (plist-get default-config :settings))
+         )
+
+    (plist-put settings :pythonPath (executable-find "python"))
+
+    (make-directory (file-name-directory custom-config) t)
+
+    (with-temp-file custom-config
+      (insert (json-encode default-config)))
+
+    custom-config))
+
+(add-hook 'python-mode-hook (lambda () (setq-local lsp-bridge-get-single-lang-server-by-project 'local/lsp-bridge-get-single-lang-server-by-project)))
+
+(add-hook 'pyvenv-post-activate-hooks
+          (lambda ()
+            (lsp-bridge-restart-process)))
 
 ;; (use-package lsp-pyright
 ;;   :ensure t
