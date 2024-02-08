63079899ca0b619b0db8882a011a7f9d6a1ddc8a
MirageTurtle <mirageturtle@gmail.com>
2024-02-08 18:45:59 +0800

    Add some global kbd as macOS preferences.
    Use Super key to do something, e.g., s-a for selecting all.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -49,6 +49,14 @@
 (global-set-key (kbd "M-SPC") 'rectangle-mark-mode)
 (global-set-key (kbd "C--") 'undo)
 
+;; macOS preferences
+(global-set-key (kbd "s-a") 'mark-whole-buffer)
+(global-set-key (kbd "s-c") 'kill-ring-save)
+(global-set-key (kbd "s-s") 'save-buffer)
+(global-set-key (kbd "s-v") 'yank)
+(global-set-key (kbd "s-z") 'undo)
+(global-set-key (kbd "s-x") 'kill-region)
+
 ;; (setq package-install-upgrade-built-in t)
 
 ;; package
