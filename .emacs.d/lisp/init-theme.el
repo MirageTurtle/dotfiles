cbea79c3b858b96f914c4079e7c598aab51ab034
MirageTurtle <mirageturtle@gmail.com>
2024-02-19 10:45:15 +0800

    Change doom-themes-treemacs-theme to `doom-colors`.

--- .emacs.d/lisp/init-theme.el
+++ .emacs.d/lisp/init-theme.el
@@ -16,7 +16,7 @@
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   ;; (doom-themes-neotree-config)
   ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (setq doom-themes-treemacs-theme "doom-colors") ; use "doom-colors" for less minimal icon theme
   (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
