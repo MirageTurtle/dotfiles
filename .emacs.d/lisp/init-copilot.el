2e4a80c8547b6dde8f503da6abe333e34d1fda22
MirageTurtle <mirageturtle@gmail.com>
2024-02-28 15:09:14 +0800

    Disable copilot feature by lsp-bridge and use a dependent copilot package.

---
+++ .emacs.d/lisp/init-copilot.el
@@ -0,0 +1,21 @@
+;;; init-copilot.el --- Copilot configuration -*- lexical-binding: t -*-
+
+(use-package editorconfig
+  :ensure t)
+
+(use-package jsonrpc
+  :ensure t)
+
+(use-package copilot
+  :straight (:host github :repo "copilot-emacs/copilot.el" :files ("dist" "*.el"))
+  :ensure t
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
+
+(provide 'init-copilot)
+
+;;; init-copilot.el ends here
