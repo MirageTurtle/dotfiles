d1b8d0866ecf388c8f0ea279195308b25565cc08
MirageTurtle <mirageturtle@gmail.com>
2024-02-28 16:00:07 +0800

    Enhance C-x 3 with my preference.

--- .emacs.d/lisp/init-efficiency.el
+++ .emacs.d/lisp/init-efficiency.el
@@ -7,6 +7,14 @@
   ("C-a" . mwim-beginning-of-code-or-line)
   ("C-e" . mwim-end-of-code-or-line))
 
+;; customed split window function
+(defun split-window-right-new ()
+  (interactive)
+  (split-window-right)
+  (other-window 1)
+  (call-interactively 'switch-to-buffer))
+(global-set-key (kbd "C-x 3") 'split-window-right-new)
+
 (provide 'init-efficiency)
 
 ;;; init-efficiency.el ends here.
