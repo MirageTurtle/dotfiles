b7d8acaf2a3444f8fcccfe3eb0050b429bc14650
MirageTurtle <mirageturtle@gmail.com>
2024-03-01 14:33:45 +0800

    Set the size of headline in org-mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -119,6 +119,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit outline-1 :height 1.728)))) 
+ '(org-level-2 ((t (:inherit outline-2 :height 1.44))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+ ;; '(org-level-1 ((t (:inherit outline-1 :height 1.728  :foreground "#FD971F"))))
+ ;; '(org-level-2 ((t (:inherit outline-2 :height 1.44  :foreground "#A6E22E"))))
+ ;; '(org-level-3 ((t (:inherit outline-3 :height 1.2  :foreground "#66D9EF"))))
+ ;; '(org-level-4 ((t (:inherit outline-4 :height 1.1  :foreground "#E6DB74"))))
+ ;; '(org-level-5 ((t (:inherit outline-5 :height 1.2  :foreground "#A1EFE4"))))
+ ;; '(org-level-6 ((t (:inherit outline-6 :height 1.2  :foreground "#A6E22E"))))
+ ;; '(org-level-7 ((t (:inherit outline-7 :height 1.2  :foreground "#F92672"))))
+ ;; '(org-level-8 ((t (:inherit outline-8 :height 1.2  :foreground "#66D9EF"))))
  )
 
 (eval-when-compile
