5ef3447728f5618b43cf3fc8e6d0fc13921c70cf
MirageTurtle <mirageturtle@gmail.com>
2024-04-03 00:25:16 +0800

    Fix bug of unmatched bracket or quote.

--- .emacs.d/lisp/init-workspace.el
+++ .emacs.d/lisp/init-workspace.el
@@ -29,7 +29,8 @@
         ("C-x t t"   . treemacs)
         ("C-x t B"   . treemacs-bookmark)
         ;; ("C-x t C-t" . treemacs-find-file)
-        ;; ("C-x t M-t" . treemacs-find-tag))
+        ;; ("C-x t M-t" . treemacs-find-tag)
+	)
   (:map treemacs-mode-map
 	("/" . treemacs-advanced-helpful-hydra)
 	("n" . treemacs-next-neighbour)
