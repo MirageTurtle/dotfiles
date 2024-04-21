0de5ff29a554f374c9c9cd72a6e0edc5bcbe3f5f
MirageTurtle <mirageturtle@gmail.com>
2024-04-21 22:48:53 +0800

    (hammerspoon)auto mount/u- sshfs after awake/sleep
    The previous implementation was buggy

--- .hammerspoon/module/majordomo.lua
+++ .hammerspoon/module/majordomo.lua
@@ -1,23 +1,45 @@
 -- A majordomo, or butler/housekeeper.
+local logger = hs.logger.new('majordomo', 'info')
 
--- post-wake commands
-postWakeCommands = {
+-- post-awake commands
+postAwakeCommands = {
    "mtsshfs vlab:/home/vlab/Documents/logseq ~/Documents/sshfs-client/logseq",
-   "mtsshfs vlab:/home/vlab/Documents/org ~/Documents/sshfs-client/org"
+   -- "mtsshfs vlab:/home/vlab/Documents/org ~/Documents/sshfs-client/org",
 }
 
-function postWake()
-   for i, command in ipairs(postWakeCommands) do
-      hs.execute(command)
+-- post-sleep commands
+postSleepCommands = {
+    -- "mysshfs-umount-all"
+   "mysshfs-umount ~/Documents/sshfs-client/logseq",
+   "mysshfs-umount ~/Documents/sshfs-client/org",
+}
+
+function postAwake()
+   for i, command in ipairs(postAwakeCommands) do
+      -- execute the command with user shell environment
+      hs.execute(command, true)
    end
 end
 
-hs.caffeinate.watcher.new(
-   function(event)
-      if event == hs.caffeinate.watcher.systemDidWake then
-	 postWake()
-	 hs.notify.new({title="Majordomo", informativeText="System did wake"}):send()
-      end
+function postSleep()
+   for i, command in ipairs(postSleepCommands) do
+      hs.execute(command, true)
    end
-):start()
+end
 
+-- https://gist.github.com/dropmeaword/cbf03189897733cbef6267dfbf36861d
+function sleepWatcher(eventType)
+   if (eventType == hs.caffeinate.watcher.systemWillSleep) then
+      -- hs.alert.show("Going to sleep!")
+      postSleep()
+      -- hs.notify.new({title="Hammerspoon", informativeText="Post-sleep commands executed."}):send()
+      -- just hammerspoon log, instead of notification
+      logger.i("Post-sleep commands executed.")
+   elseif (eventType == hs.caffeinate.watcher.systemDidWake) then
+      -- hs.alert.show("Waking up!")
+      postAwake()
+      hs.notify.new({title="Hammerspoon", informativeText="Post-awake commands executed."}):send()
+   end
+end
+
+hs.caffeinate.watcher.new(sleepWatcher):start()
