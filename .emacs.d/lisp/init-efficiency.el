dedd41813ca9635b5773d17fc8a47edfa4f64fb9
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2024-07-07 20:21:09 +0800

    Use awesome-tab as tab tool.

--- .emacs.d/lisp/init-efficiency.el
+++ .emacs.d/lisp/init-efficiency.el
@@ -36,6 +36,63 @@
   ("C-<" . mc/mark-previous-like-this)
   ("C-c C-<" . mc/mark-all-like-this))
 
+;; awesome-tab
+(use-package awesome-tab
+  :straight (awesome-tab :type git :host github :repo "manateelazycat/awesome-tab")
+  :ensure t
+  :config
+  (awesome-tab-mode t))
+(defhydra awesome-fast-switch (:hint nil)
+  "
+ ^^^^Fast Move             ^^^^Tab                    ^^Search            ^^Misc
+-^^^^--------------------+-^^^^---------------------+-^^----------------+-^^---------------------------
+   ^_k_^   prev group    | _C-a_^^     select first | _b_ search buffer | _C-k_   kill buffer
+ _h_   _l_  switch tab   | _C-e_^^     select last  | _g_ search group  | _C-S-k_ kill others in group
+   ^_j_^   next group    | _C-j_^^     ace jump     | ^^                | ^^
+ ^^0 ~ 9^^ select window | _C-h_/_C-l_ move current | ^^                | ^^
+-^^^^--------------------+-^^^^---------------------+-^^----------------+-^^---------------------------
+"
+  ("h" awesome-tab-backward-tab)
+  ("j" awesome-tab-forward-group)
+  ("k" awesome-tab-backward-group)
+  ("l" awesome-tab-forward-tab)
+  ;; ("0" my-select-window)
+  ;; ("1" my-select-window)
+  ;; ("2" my-select-window)
+  ;; ("3" my-select-window)
+  ;; ("4" my-select-window)
+  ;; ("5" my-select-window)
+  ;; ("6" my-select-window)
+  ;; ("7" my-select-window)
+  ;; ("8" my-select-window)
+  ;; ("9" my-select-window)
+  ("C-a" awesome-tab-select-beg-tab)
+  ("C-e" awesome-tab-select-end-tab)
+  ("C-j" awesome-tab-ace-jump)
+  ("C-h" awesome-tab-move-current-tab-to-left)
+  ("C-l" awesome-tab-move-current-tab-to-right)
+  ("b" ivy-switch-buffer)
+  ("g" awesome-tab-counsel-switch-group)
+  ("C-k" kill-current-buffer)
+  ("C-S-k" awesome-tab-kill-other-buffers-in-current-group)
+  ("q" nil "quit"))
+;; winum users can use `winum-select-window-by-number' directly.
+(defun my-select-window-by-number (win-id)
+  "Use `ace-window' to select the window by using window index.
+WIN-ID : Window index."
+  (let ((wnd (nth (- win-id 1) (aw-window-list))))
+    (if wnd
+        (aw-switch-to-window wnd)
+      (message "No such window."))))
+(defun my-select-window ()
+  (interactive)
+  (let* ((event last-input-event)
+         (key (make-vector 1 event))
+         (key-desc (key-description key)))
+    (my-select-window-by-number
+     (string-to-number (car (nreverse (split-string key-desc "-")))))))
+(global-set-key (kbd "C-x t w") 'awesome-fast-switch/body)
+
 (provide 'init-efficiency)
 
 ;;; init-efficiency.el ends here.
