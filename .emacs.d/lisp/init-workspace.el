aa1d23e51252a7f56187c068a2bafc00b75af555
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2024-07-25 16:33:22 +0800

    Use neo-tree instead of treemacs for sidebar.

--- .emacs.d/lisp/init-workspace.el
+++ .emacs.d/lisp/init-workspace.el
@@ -12,34 +12,39 @@
   :after (projectile)
   :init (counsel-projectile-mode))
 
-(use-package treemacs
+;; (use-package treemacs
+;;   :ensure t
+;;   :defer t
+;;   :config
+;;   (treemacs-tag-follow-mode)
+;;   (treemacs-follow-mode)
+;;   (treemacs-filewatch-mode)
+;;   ;; (treemacs-git-mode 'deferred)
+;;   (use-package treemacs-icons-dired
+;;     :ensure t
+;;     :after (treemacs dired)
+;;     :config
+;;     (treemacs-icons-dired-mode))
+;;   (setf treemacs-select-when-already-in-treemacs 'stay)
+;;   :bind
+;;   (:map global-map
+;;         ("M-0"       . treemacs-select-window)
+;;         ("C-x t 1"   . treemacs-delete-other-windows)
+;;         ("C-x t t"   . treemacs)
+;;         ("C-x t B"   . treemacs-bookmark)
+;;         ;; ("C-x t C-t" . treemacs-find-file)
+;;         ;; ("C-x t M-t" . treemacs-find-tag)
+;; 	)
+;;   (:map treemacs-mode-map
+;; 	("/" . treemacs-advanced-helpful-hydra)
+;; 	("n" . treemacs-next-neighbour)
+;; 	("p" . treemacs-previous-neighbour)
+;; 	("r" . treemacs-rename-file)))
+
+(use-package neotree
   :ensure t
-  :defer t
   :config
-  (treemacs-tag-follow-mode)
-  (treemacs-follow-mode)
-  (treemacs-filewatch-mode)
-  ;; (treemacs-git-mode 'deferred)
-  (use-package treemacs-icons-dired
-    :ensure t
-    :after (treemacs dired)
-    :config
-    (treemacs-icons-dired-mode))
-  (setf treemacs-select-when-already-in-treemacs 'stay)
-  :bind
-  (:map global-map
-        ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ("C-x t B"   . treemacs-bookmark)
-        ;; ("C-x t C-t" . treemacs-find-file)
-        ;; ("C-x t M-t" . treemacs-find-tag)
-	)
-  (:map treemacs-mode-map
-	("/" . treemacs-advanced-helpful-hydra)
-	("n" . treemacs-next-neighbour)
-	("p" . treemacs-previous-neighbour)
-	("r" . treemacs-rename-file)))
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
 
 (use-package treemacs-projectile
   :ensure t
