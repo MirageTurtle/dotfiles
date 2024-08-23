be201a2b52710dd23da4bec253b3ef6250149e59
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2024-08-24 03:22:49 +0800

    Update dirvish configuration.
    disable `dirvish-side`; move vc-state to the first; disable header-line
    and mode-line for integrating with awesome-tray; change the keybinding
    for `dirvish-layout-toggle`

--- .emacs.d/lisp/init-dirvish.el
+++ .emacs.d/lisp/init-dirvish.el
@@ -14,16 +14,19 @@
   (when (eq system-type 'darwin)
     (setq insert-directory-program "gls"))
   ;; (dirvish-peek-mode) ; Preview files in minibuffer
-  (dirvish-side)
+  ;; (dirvish-side)
   (dirvish-side-follow-mode) ; similar to `treemacs-follow-mode'
   (setq dirvish-mode-line-format
         '(:left (sort symlink) :right (omit yank index)))
   (setq dirvish-attributes
-        '(nerd-icons file-time file-size collapse subtree-state vc-state git-msg))
+        '(vc-state nerd-icons file-time file-size collapse subtree-state git-msg))
   (setq dirvish-subtree-state-style 'nerd)
   (setq delete-by-moving-to-trash t)
   (setq dired-listing-switches
         "-l --almost-all --human-readable --group-directories-first --no-group")
+  ;; integrating with awesome-tray
+  (setq dirvish-use-header-line nil)     ; hide header line (show the classic dired header)
+  (setq dirvish-use-mode-line nil)       ; hide mode line
   :bind ; Bind `dirvish|dirvish-side|dirvish-dwim' as you see fit
   (("C-c f" . dirvish-fd)
    :map dirvish-mode-map ; Dirvish inherits `dired-mode-map'
@@ -40,7 +43,7 @@
    ("M-b" . dirvish-history-go-backward)
    ("M-l" . dirvish-ls-switches-menu)
    ("M-m" . dirvish-mark-menu)
-   ("M-t" . dirvish-layout-toggle)
+   ("t" . dirvish-layout-toggle)
    ("M-s" . dirvish-setup-menu)
    ("M-e" . dirvish-emerge-menu)
    ("M-j" . dirvish-fd-jump)))
