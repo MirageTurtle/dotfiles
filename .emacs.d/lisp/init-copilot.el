aa66ebc1e517dbdde4d883f3fe52996bed5e5be9
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2024-09-03 15:16:52 +0800

    chore: (emacs)enable copilot package.

--- .emacs.d/lisp/init-copilot.el
+++ .emacs.d/lisp/init-copilot.el
@@ -6,19 +6,19 @@
 (use-package jsonrpc
   :ensure t)
 
-;; (use-package copilot
-;;   :straight (:host github :repo "copilot-emacs/copilot.el" :files ("dist" "*.el"))
-;;   :ensure t
-;;   :hook (prog-mode . copilot-mode)
-;;   :config
-;;   (setq copilot-indent-offset-warning-disable t)
-;;   :bind (:map copilot-completion-map
-;;               ;; ("<tab>" . 'copilot-accept-completion)
-;;               ;; ("TAB" . 'copilot-accept-completion)
-;; 	      ("C-e" . 'copilot-accept-completion)
-;;               ;; ("C-TAB" . 'copilot-accept-completion-by-word)
-;;               ;; ("C-<tab>" . 'copilot-accept-completion-by-word)
-;; 	      ("M-f" . 'copilot-accept-completion-by-word)))
+(use-package copilot
+  :straight (:host github :repo "copilot-emacs/copilot.el" :files ("dist" "*.el"))
+  :ensure t
+  :hook (prog-mode . copilot-mode)
+  :config
+  (setq copilot-indent-offset-warning-disable t)
+  :bind (:map copilot-completion-map
+              ;; ("<tab>" . 'copilot-accept-completion)
+              ;; ("TAB" . 'copilot-accept-completion)
+	      ("C-e" . 'copilot-accept-completion)
+              ;; ("C-TAB" . 'copilot-accept-completion-by-word)
+              ;; ("C-<tab>" . 'copilot-accept-completion-by-word)
+	      ("M-f" . 'copilot-accept-completion-by-word)))
 
 (provide 'init-copilot)
 
