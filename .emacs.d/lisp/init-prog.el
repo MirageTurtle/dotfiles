5345e71e98b5ffbb0e8360cc78817db1fcff8fd3
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2024-11-08 14:20:50 +0800

    feat(emacs): use dape for debugging, ts-fold for folding.

--- .emacs.d/lisp/init-prog.el
+++ .emacs.d/lisp/init-prog.el
@@ -3,6 +3,17 @@
 (use-package reformatter
   :ensure t)
 
+(use-package ts-fold
+  :ensure t
+  :straight (ts-fold :type git :host github :repo "emacs-tree-sitter/ts-fold")
+  :hook (prog-mode . ts-fold-mode))
+
+;; debug
+(use-package dape
+  ;; require jsonrpc >= 1.0.24, I use 1.0.25
+  :config
+  (setq dape-buffer-window-arrangement 'right))
+
 (provide 'init-prog)
 
 ;;; init-prog.el ends here
