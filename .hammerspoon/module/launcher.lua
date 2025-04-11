3d224bf0915a3f62cf639015b3642c71002c6c68
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2025-04-11 17:13:55 +0800

    chore(hammerspoon): use Cmd+Alt as modified keys instead of Cmd+Shift
    Not useing Cmd+Shift+<shortcut> because it is often used by other
    purpose (e.g., Cmd+Shift+F to find globally).

--- .hammerspoon/module/launcher.lua
+++ .hammerspoon/module/launcher.lua
@@ -1,13 +1,16 @@
 local applist = {
-    {shortcut = 'F', appname = 'Emacs'},
+    {shortcut = 'H', appname = 'Emacs'},
     {shortcut = 'J', appname = 'Alacritty'},
-    {shortcut = 'O', appname = 'Logseq'},
-    {shortcut = 'D', appname = 'Dictionary'},
-    {shortcut = 'C', appname = "Chromium"},
+    -- {shortcut = 'O', appname = 'Logseq'},
+    -- {shortcut = 'D', appname = 'Dictionary'},
+    {shortcut = 'N', appname = "Chromium"},
 }
 
+-- Use Cmd+Alt+<shortcut> to launch or focus the app
+-- Not useing Cmd+Shift+<shortcut> because it is often used by other purpose
+-- (e.g., Cmd+Shift+F to find globally)
 hs.fnutils.each(applist, function (entry)
-    hs.hotkey.bind({'cmd', 'shift'}, entry.shortcut, entry.appname, function ()
+    hs.hotkey.bind({'cmd', 'alt'}, entry.shortcut, entry.appname, function ()
         hs.application.launchOrFocus(entry.appname)
     end)
 end)
