3d1aa505845090804005c7679d3a1ec5b97d26bc
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2025-04-19 00:09:29 +0800

    feat: add menu for amphetamin, and enable timeout feature.

--- .hammerspoon/module/amphetamine.lua
+++ .hammerspoon/module/amphetamine.lua
@@ -1,9 +1,6 @@
 -- A substitute for Amphetamine.app
 -- All the icons are from https://www.iconfont.cn/search/index?searchType=icon&q=caffeine&page=1&fromCollection=-1
 
--- TODO:
--- 1. Add a timer to keep awake for a certain time.
-
 local amphetamineIcon = {
    no_amphetamine = hs.image.imageFromPath('assets/amphetamine/no-caffeine.png'):setSize({ w = 20, h = 20 }),
    amphetamine = hs.image.imageFromPath('assets/amphetamine/coffee.png'):setSize({ w = 20, h = 20 }),
@@ -19,5 +16,28 @@ local function toggleAmphetamine()
    end
 end
 
+local function enableAmphetamineWithTimeout(timeout)
+   hs.caffeinate.set('displayIdle', true)
+   amphetamineBar:setIcon(amphetamineIcon['amphetamine'])
+   hs.timer.doAfter(timeout, function()
+      hs.caffeinate.set('displayIdle', false)
+      amphetamineBar:setIcon(amphetamineIcon['no_amphetamine'])
+   end)
+end
+
 amphetamineBar:setIcon(amphetamineIcon['no_amphetamine'])
-amphetamineBar:setClickCallback(toggleAmphetamine)
+-- amphetamineBar:setClickCallback(toggleAmphetamine)
+amphetamineBar:setMenu({
+   { title = 'Toggle', fn = function() toggleAmphetamine() end },
+   { title = 'Enable for 5 minutes', fn = function() enableAmphetamineWithTimeout(300) end },
+   { title = 'Enable for 15 minutes', fn = function() enableAmphetamineWithTimeout(900) end },
+   { title = 'Enable for 30 minutes', fn = function() enableAmphetamineWithTimeout(1800) end },
+   { title = 'Enable for 1 hour', fn = function() enableAmphetamineWithTimeout(3600) end },
+   { title = 'Enable for 2 hours', fn = function() enableAmphetamineWithTimeout(7200) end },
+   { title = 'Enable for 4 hours', fn = function() enableAmphetamineWithTimeout(14400) end },
+   { title = 'Enable for 8 hours', fn = function() enableAmphetamineWithTimeout(28800) end },
+   { title = 'Enable for 12 hours', fn = function() enableAmphetamineWithTimeout(43200) end },
+   { title = 'Enable for 24 hours', fn = function() enableAmphetamineWithTimeout(86400) end },
+   { title = '-' },
+   -- { title = 'Disable', fn = function() hs.caffeinate.set('displayIdle', false) amphetamineBar:setIcon(amphetamineIcon['no_amphetamine']) end },
+})
